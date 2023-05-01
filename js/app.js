95c27245a9ddddf4d583d284a961fe94646b4fd0
Mikhail <potaomg@gmail.com>
2023-05-02 00:53:35 +0300

    feat: add animation of pressing a key is implemented

--- js/app.js
+++ js/app.js
@@ -6,7 +6,7 @@
 // }))
 
 
-const arr = ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace', 'Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\', 'Delete', 'CapsLock','a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', "'", 'Enter', 'Shift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', 'ArrowUp', 'Shift', 'Control', 'Meta', 'Alt', ' ', 'Alt','Control', 'ArrowLeft',  'ArrowDown', 'ArrowRight'];
+const arr = ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace', 'Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\', 'Delete', 'CapsLock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', "'", 'Enter', 'Shift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', 'ArrowUp', 'Shift', 'Control', 'Meta', 'Alt', ' ', 'Alt', 'Control', 'ArrowLeft', 'ArrowDown', 'ArrowRight'];
 
 let arrRus = ['ё', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace', 'Tab', 'й', 'ц', 'у', 'к', 'е', 'н', 'г', 'ш', 'щ', 'з', 'х', 'ъ', '\\', 'Delete', 'CapsLock', 'ф', 'ы', 'в', 'а', 'п', 'р', 'о', 'л', 'д', 'ж', 'э', 'Enter', 'Shift', 'я', 'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 'ю', '.', 'ArrowUp', 'Shift', 'Control', 'Meta', 'Alt', ' ', 'Alt', 'Control', 'ArrowLeft', 'ArrowDown', 'ArrowRight'];
 
@@ -16,6 +16,7 @@ const pageDom = () => {
   <input class="input" type ="text" value = "">
   <div class="keyboard">
   </div>
+  <p>язык меняется клавишами Alt + Shift левые. есть баг начальная раскладка должна совпадать с вашим текущим языком</p>
 </div>`;
 };
 pageDom();
@@ -27,98 +28,109 @@ const keyBoard = document.querySelector(".keyboard");
 
 
 let arrCurent = arr;
-const init = () =>{
+const init = () => {
 
   let out = "";
-  for (let i = 0; i < arr.length; i++){
-    if( i == 0){
+  for (let i = 0; i < arr.length; i++) {
+    if (i == 0) {
       out += `<div class = "key dark" data ="${arrCurent[i]}">${arrCurent[i]}</div>`;
     }
-    else if(arrCurent[i] === "ArrowUp"){
+    else if (arrCurent[i] === "ArrowUp") {
       out += `<div class = "key dark padding-none" data ="${arrCurent[i]}">${'<img src="./asset/up.png"  class="img "></img>'}</div>`;
     }
-    else if(arrCurent[i] === "ArrowDown"){
+    else if (arrCurent[i] === "ArrowDown") {
       out += `<div class = "key dark dawn padding-none" data ="${arrCurent[i]}">${'<img src="./asset/up.png"  class="img "></img>'}</div>`;
     }
-    else if(arrCurent[i] === "ArrowLeft"){
+    else if (arrCurent[i] === "ArrowLeft") {
       out += `<div class = "key dark left padding-none" data ="${arrCurent[i]}">${'<img src="./asset/up.png"  class="img "></img>'}</div>`;
     }
-    else if(arrCurent[i] === "ArrowRight"){
+    else if (arrCurent[i] === "ArrowRight") {
       out += `<div class = "key dark rigth padding-none" data ="${arrCurent[i]}">${'<img src="./asset/up.png"  class="img "></img>'}</div>`;
     }
-    else if(arrCurent[i] === "Delete"){
+    else if (arrCurent[i] === "Delete") {
       out += `<div class = "key dark big-key" data ="${arrCurent[i]}">${'Del'}</div>`;
     }
-    else if(arrCurent[i] === "Meta"){
+    else if (arrCurent[i] === "Meta") {
       out += `<div class = "key dark big-key" data ="${arrCurent[i]}">${'Win'}</div>`;
     }
-    else if(arrCurent[i] === "Control"){
+    else if (arrCurent[i] === "Control") {
       out += `<div class = "key dark big-key" data ="${arrCurent[i]}">${'Ctrl'}</div>`;
     }
-    else if(arrCurent[i] === " "){
+    else if (arrCurent[i] === " ") {
       out += `<div class = "key spaes" data ="${arrCurent[i]}">${arrCurent[i]}</div>`;
     }
-    else if(arrCurent[i].length == 3){
+    else if (arrCurent[i].length == 3) {
       out += `<div class = "key dark big-key" data ="${arrCurent[i]}">${arrCurent[i]}</div>`;
-    }else if(arrCurent[i].length == 4){
+    } else if (arrCurent[i].length == 4) {
       out += `<div class = "key dark ctrl" data ="${arrCurent[i]}">${arrCurent[i]}</div>`;
     }
-    else if(arrCurent[i].length > 3){
+    else if (arrCurent[i].length > 3) {
       out += `<div class = "key dark shift" data ="${arrCurent[i]}">${arrCurent[i]}</div>`;
     }
-    else{ out += `<div class="key upperCase" data ="${arrCurent[i]}" >${arrCurent[i]}</div>`;
-  }
+    else {
+      out += `<div class="key upperCase" data ="${arrCurent[i]}" >${arrCurent[i]}</div>`;
+    }
   }
 
   keyBoard.innerHTML = out;
 
 };
 
-  init();
+init();
 
 const keyBotton = keyBoard.querySelectorAll(".key"),
-      input = document.querySelector(".input");
+  input = document.querySelector(".input");
 
-  // 
-document.onkeydown = function(event){
 
-  keyBotton.forEach( function(item){
+const removeKey = () => {
+  keyBotton.forEach(function (item) {
     item.classList.remove("active");
   });
+};
 
-  if(event.key === 'Backspace' || event.key ==='Tab' || event.key === 'ArrowUp'|| event.key === 'Shift' || event.key === 'Control' || event.key ==='Meta' || event.key ==='Alt' || event.key === 'ArrowLeft' || event.key ==='ArrowDown' || event.key ==='ArrowRight' || event.key === 'Enter' || event.key === 'CapsLock' || event.key === 'Delete' || event.key === ' '){
+document.onkeydown = function (event) {
 
-    keyBoard.querySelector('[data = "'+event.key+'"]').classList.add('active');
+  removeKey();
 
-  }else {
-    keyBoard.querySelector('[data = "'+event.key.toLowerCase()+'"]').classList.add('active');
+  if (event.key === 'Backspace' || event.key === 'Tab' || event.key === 'ArrowUp' || event.key === 'Shift' || event.key === 'Control' || event.key === 'Meta' || event.key === 'Alt' || event.key === 'ArrowLeft' || event.key === 'ArrowDown' || event.key === 'ArrowRight' || event.key === 'Enter' || event.key === 'CapsLock' || event.key === 'Delete' || event.key === ' ') {
+
+    keyBoard.querySelector('[data = "' + event.key + '"]').classList.add('active');
+
+    setTimeout(removeKey, 200);
+
+  } else {
+    keyBoard.querySelector('[data = "' + event.key.toLowerCase() + '"]').classList.add('active');
+
+    setTimeout(removeKey, 200);
+
   }
 
   input.value += event.key;
 };
 
-keyBotton.forEach( (item) =>{
-  item.onclick = function(){
-    keyBotton.forEach(function(e){
-      e.classList.remove('active');
+keyBotton.forEach((item) => {
+  item.onclick = function () {
+    keyBotton.forEach(function () {
+      removeKey();
     });
     let codeKey = this.getAttribute('data');
     this.classList.add('active');
+    setTimeout(removeKey, 200);
     input.value += codeKey;
-    
+
   };
 });
 
 
 
 const changeKeyboard = (e) => {
-  if(e.code === "AltLeft"){
-    document.onkeyup = function(e){
-      if(e.code === "ShiftLeft"){
-        if(arrCurent === arr){
+  if (e.code === "AltLeft") {
+    document.onkeyup = function (e) {
+      if (e.code === "ShiftLeft") {
+        if (arrCurent === arr) {
           arrCurent = arrRus;
           init();
-        }else{
+        } else {
           arrCurent = arr;
           init();
         }
@@ -126,6 +138,8 @@ const changeKeyboard = (e) => {
     };
   }
 };
+
+
 document.addEventListener('keydown', changeKeyboard);
 
 
